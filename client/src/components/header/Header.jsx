8d1a5e1a4aadf64bf85b3aa7bd70be2df3f2a90b
Silvija96 <silvija.barkauskaite064@gmail.com>
2025-06-26 13:20:42 +0300

    user context

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router";
 import { MenuLink } from "./MenuLink";
+import { mainMenuData } from "../../data/mainMenuData";
+import heroImg from '../../assets/movies-hero.png';
 
 export function Header() {
     return (
@@ -7,17 +9,18 @@ export function Header() {
             <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
                 <div className="col-md-3 mb-2 mb-md-0">
                     <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
-                        <img src="#" alt="Logo" />
+                        <img src={heroImg} alt="Logo" style={{ height: '3rem' }} />
                     </a>
                 </div>
                 <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                    <MenuLink to='/'>Home</MenuLink>
-                    <MenuLink to='/movies'>Movies</MenuLink>
-                    <MenuLink to='/categories'>Categories</MenuLink>
+                    {mainMenuData.map(link => <MenuLink key={link.text} to={link.href}>{link.text}</MenuLink>)}
                 </ul>
                 <div className="col-md-3 text-end">
                     <Link to='/register' className="btn btn-primary me-2">Register</Link>
                     <Link to='/login' className="btn btn-outline-primary">Login</Link>
+                </div>
+                <div className="col-md-3 text-end">
+                    <Link to='/dashboard' className="btn btn-primary me-2">Dashboard</Link>
                 </div>
             </header>
         </div>
